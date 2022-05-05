9f5ef3379f832d6cac9a23c1129becdcc3cbcf18
Carlos Gueiros <carlosbgueiros@gmail.com>
2022-05-05 13:08:22 +0200

    Include data and folder structure

---
+++ scripts/01_load_data.R
@@ -0,0 +1,20 @@
+
+#library(data.table)
+library(dplyr)
+#library(tidyr)
+#library(purrr)
+#library(glue)
+library(haven)
+
+ineq_pol_data <- read_dta("raw-data/swissubase_1323_1_0/1323_Ineq-pol_Data_v1.0.0.dta")
+
+#dplyr::glimpse(ineq_pol_data)
+
+
+
+
+
+
+
+
+
